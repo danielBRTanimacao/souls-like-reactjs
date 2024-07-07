92d69e9e66de58959e5556cb91514669b8372d41
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-07 20:57:29 -0300

    feature on header

--- souls-like/src/components/Header.jsx
+++ souls-like/src/components/Header.jsx
@@ -1,7 +1,31 @@
 export default () => {
     return (
         <>
-            <header>creating a header</header>
+            <header className="container nav">
+                <h3>
+                    <a href="/">Icon</a>
+                </h3>
+                <div class="dropdown">
+                    <button class="dropbtn btn">Burger</button>
+                    <ul class="dropdown-content">
+                        <li>
+                            <a href="https://github.com/danielBRTanimacao">
+                                GitHub
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/daniel-tenório-6471b0244/">
+                                Linkedin
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UCS88R5WOovJNe3NtS0OhUfg">
+                                Youtube
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
         </>
     );
 };
