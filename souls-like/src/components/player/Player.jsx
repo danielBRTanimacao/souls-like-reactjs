0a3832bd36bc213b0a9c0391a35379619b9001c2
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-09 16:00:40 -0300

    making logics to player move

--- souls-like/src/components/player/Player.jsx
+++ souls-like/src/components/player/Player.jsx
@@ -1,11 +1,33 @@
+import React from "react";
+import { useState } from "react";
+
 export default () => {
+    const [initPos, setInitPos] = useState(0);
+
     function movePlayer(e) {
         console.log(e);
+        setInitPos(200);
     }
 
     return (
         <>
-            <div className="player" onClick={(e) => movePlayer(e)}>
+            <div
+                // className="player"
+                onClick={(e) => movePlayer(e)}
+                style={{
+                    position: "relative",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    top: `${initPos}px`,
+                    left: `${initPos}px`,
+                    width: "60px",
+                    height: "60px",
+                    backgroundColor: "red",
+                    borderRadius: "100%",
+                    cursor: "pointer"
+                }}
+            >
                 <span hidden>Player</span>
             </div>
         </>
