8dd16fc7d57ebf349e55c0055c0a57803fb4398f
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-10 13:58:20 -0300

    trying to resolve bug person

--- souls-like/src/components/player/Player.jsx
+++ souls-like/src/components/player/Player.jsx
@@ -2,24 +2,34 @@ import React from "react";
 import { useState } from "react";
 
 export default () => {
-    const [initPos, setInitPos] = useState(0);
+    const [topPos, settopPos] = useState(0);
+    const [leftPos, setleftPos] = useState(0);
 
-    function movePlayer(e) {
-        console.log(e);
-        setInitPos(200);
-    }
+    const maxScreenWidth = window.innerWidth;
+
+    addEventListener("keydown", (event) => {
+        if (event.key == "w") {
+            settopPos((pos) => (pos += 1));
+            // settopPos(Math.floor(Math.random() * (200 - -200 + 1)) + -200);
+        } else if (event.key == "s") {
+            settopPos((pos) => (pos -= 1));
+        } else if (event.key == "d") {
+            setleftPos((pos) => (pos += 1));
+        } else if (event.key == "a") {
+            setleftPos((pos) => (pos -= 1));
+        }
+    });
 
     return (
         <>
             <div
-                onClick={(e) => movePlayer(e)}
                 style={{
                     position: "relative",
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
-                    top: `${initPos}px`,
-                    left: `${initPos}px`,
+                    top: `${topPos}px`,
+                    left: `${leftPos}px`,
                     width: "60px",
                     height: "60px",
                     backgroundColor: "red",
