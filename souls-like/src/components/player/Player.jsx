ea5e54e512367d673950f00320f4c5086e22b1cd
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-12 14:25:06 -0300

    add all design base on player

--- souls-like/src/components/player/Player.jsx
+++ souls-like/src/components/player/Player.jsx
@@ -1,24 +1,34 @@
 import React, { useState, useEffect } from "react";
 import Knight from "../../assets/player-sprite/knight-idle.png";
+import KnightUp from "../../assets/player-sprite/knight-up.png";
+import KnightDown from "../../assets/player-sprite/knight-down.png";
 
 export default () => {
+    const [imgVariant, setImgVariant] = useState(Knight);
+    const [invertImg, setInvertImg] = useState("1");
     const [statePos, setstatePos] = useState({ x: 0, y: 0 });
 
-    // const maxScreenWidth = window.innerWidth;
+    const maxScreenWidth = window.innerWidth;
 
     useEffect(() => {
         window.addEventListener("keydown", (event) => {
             if (event.key == "w") {
                 setstatePos((prev) => ({ ...prev, x: prev.x + 10 }));
+                setImgVariant(KnightUp);
             }
             if (event.key == "a") {
                 setstatePos((prev) => ({ ...prev, y: prev.y - 10 }));
+                setInvertImg("1");
+                setImgVariant(Knight);
             }
             if (event.key == "s") {
                 setstatePos((prev) => ({ ...prev, x: prev.x - 10 }));
+                setImgVariant(KnightDown);
             }
             if (event.key == "d") {
                 setstatePos((prev) => ({ ...prev, y: prev.y + 10 }));
+                setInvertImg("-1");
+                setImgVariant(Knight);
             }
         });
     }, []);
@@ -31,12 +41,14 @@ export default () => {
                     bottom: `${statePos.x}px`,
                     left: `${statePos.y}px`,
                     width: "60px",
-                    height: "60px",
-                    backgroundColor: "red",
-                    borderRadius: "100%",
-                    background: `url(${Knight}) no-repeat center`
+                    height: "60px"
                 }}
             >
+                <img
+                    src={imgVariant}
+                    style={{ width: "60px", transform: `scaleX(${invertImg})` }}
+                    alt="img-knight"
+                />
                 <span hidden>Player</span>
             </div>
         </>
